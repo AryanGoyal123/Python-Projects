34ba7139a180194f35abddca1cf8a06c42e348d0
aryangoyal123 <aryanlovesmath@gmail.com>
2023-11-25 00:52:07 -0500

    lambda

---
+++ Python Practice/functions.py
@@ -0,0 +1,16 @@
+# labda functions
+add = lambda x, y: x + y
+subtract = lambda x, y: x - y
+multiply = lambda x, y: x * y
+divide = lambda x, y: x / y
+
+# map functions maps each of the values to the lambda function and then generates a new map object,
+# need to convert it back into list type
+squared_numbers = map(lambda x: x ** 2, range(5))
+print(list(squared_numbers))
+
+# filter applies the function to each of the elements in a list and selects the ones that are true
+even_numbers = list(filter(lambda x: x%2 == 0, range(10)))
+print(even_numbers)
+
+print(multiply(5,4))
