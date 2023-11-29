860ee466a4e235ceefcb0e4587bbe1d6a6a5328d
aryangoyal123 <aryanlovesmath@gmail.com>
2023-11-29 00:01:58 -0500

    teacher fixed

--- Python OOP Projects/School Management Project/Course.py
+++ Python OOP Projects/School Management Project/Course.py
@@ -1,11 +1,8 @@
-
 class Course:
-    def __init__(self, course_code, course_name, credit, student_enrolled, teachers):
-        self.student_enrolled = student_enrolled
-        self.course_code = course_code
-        self.credits = credit
-        self.course_name = course_name
-        self.teachers = teachers
+    def __init__(self, _code: int, _name: str, _credit: int):
+        self._course_code = _code
+        self._credits = _credit
+        self._course_name = _name
 
     def add_student(self, student):
         pass
@@ -15,3 +12,33 @@ class Course:
 
     def get_course_details(self):
         pass
+
+    @property
+    def code(self):
+        return self._course_code
+
+    @property
+    def credits(self):
+        return self._credits
+
+    @property
+    def name(self):
+        return self._course_name
+
+    @code.setter
+    def code(self, new_course_code: int):
+        if not isinstance(new_course_code, int) or new_course_code <= 0:
+            raise ValueError("Course code should be a positive integer")
+        self._course_code = new_course_code
+
+    @credits.setter
+    def credits(self, new_credits: int):
+        if not isinstance(new_credits, int) or new_credits <= 0:
+            raise ValueError("Course code should be a positive integer")
+        self._credits = new_credits
+
+    @name.setter
+    def name(self, new_course_name: str):
+        if not isinstance(new_course_name, str):
+            raise TypeError("Course name should be a string")
+        self._course_name = new_course_name
--- Python OOP Projects/School Management Project/Teacher.py
+++ Python OOP Projects/School Management Project/Teacher.py
@@ -2,14 +2,21 @@ from Person import Person
 
 
 class Teacher(Person):
-    def __init__(self, name, age, teacher_id, courses_taught=None):
-        super().__init__(name, age)
+    def __init__(self, _name: str, _age: int, _teacher_id: int, courses_taught=None):
+        super().__init__(_name, _age)
 
-        self.teacherID = teacher_id
+        if not isinstance(_teacher_id, int) or _teacher_id <= 0:
+            raise TypeError("Student ID should be an integer and positive")
+        else:
+            self._teacherID = _teacher_id
+
         if courses_taught is None:
             """
             load data from CSV file
             """
+
+    def __str__(self):
+        return f"Teacher(name:{self._name}, age:{self._age}, teacherID:{self._teacherID})"
 
     def enroll_course(self, course):
         pass
@@ -17,6 +24,6 @@ class Teacher(Person):
     def view_courses_taught(self):
         pass
 
-    def get_teacher_details(self):
-        super().get_person_details()
-        print(f"Teacher ID: {self.teacherID}")
+    @property
+    def teacherID(self):
+        return self._teacherID
