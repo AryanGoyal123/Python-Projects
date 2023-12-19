69fde640a90e60637c7f88c6b5098eb3f1c9535d
aryangoyal123 <aryanlovesmath@gmail.com>
2023-12-19 14:54:09 -0500

    advance type annotation

---
+++ Python Practice/adv_type_annotate.py
@@ -0,0 +1,33 @@
+from typing import List, Dict, Tuple, Optional, Union
+
+Vector = List[Union[int, float]]
+Matrix = List[List[Union[int, float]]]
+
+
+# You can use union types to specify that a variable or parameter can have multiple types
+def scale_vector(vector: Vector, factor: Union[int, float]) -> Vector:
+    return [item * factor for item in vector]
+
+
+def square_to_double(value: Union[int, float]) -> Union[int, float]:
+    return value ** 2 if isinstance(value, (int, float)) else value * 2
+
+
+def process_items(items: List[int]) -> Dict[str, Tuple[int, int]]:
+    result = {}
+    for i, item in enumerate(items):
+        result[f'item_{i}'] = (item, item * 2)
+    return result
+
+
+# The Optional type annotation is a way to express that a variable or function argument may be of a certain type or None
+def greet(name: Optional[str]) -> str:
+    if name is not None:
+        return f"Hello, {name}!"
+    return "Hello, Guest!"
+
+
+def find_element(id_list: List[int], target: int) -> Optional[int]:
+    if target in id_list:
+        return target
+    return None
--- Python Practice/rect_dataclass.py
+++ Python Practice/rect_dataclass.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
+from typing import Union
 
 
 class Shape(ABC):
@@ -17,25 +18,23 @@ class Shape(ABC):
 class Circle(Shape):
     _diameter: float = field(default=0.0)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.diameter = self._diameter
         self.radius = self._diameter / 2
 
-    def __str__(self) -> str:
-        return f"Rectangle(diameter={self.diameter}, radius={self.radius})"
-
-    def area(self) -> float:
-        return (self.radius * self.radius) * 3.14159
-
-    def perimeter(self) -> float:
-        return 2 * 3.14159 * self.radius
+    def __str__(self) -> str: return f"Rectangle(diameter={self.diameter}, radius={self.radius})"
 
     @property
-    def diameter(self) -> float:
-        return self._diameter
+    def area(self) -> float: return (self.radius * self.radius) * 3.14159
+
+    @property
+    def perimeter(self) -> float: return 2 * 3.14159 * self.radius
+
+    @property
+    def diameter(self) -> float: return self._diameter
 
     @diameter.setter
-    def diameter(self, new_diameter: float) -> None:
+    def diameter(self, new_diameter: Union[int, float]) -> None:
         if not isinstance(new_diameter, (int, float)):
             raise TypeError(f"Expected float for width, got {type(new_diameter).__name__}.")
         elif new_diameter <= 0:
@@ -52,27 +51,22 @@ class Rectangle(Shape):
         self.width = self._width
         self.height = self._height
 
-    def __str__(self) -> str:
-        return f"Rectangle(width={self.width}, height={self.height})"
+    def __str__(self) -> str: return f"Rectangle(width={self.width}, height={self.height})"
 
     @property
-    def area(self) -> float:
-        return self._height * self._width
+    def area(self) -> float: return self._height * self._width
 
     @property
-    def perimeter(self) -> float:
-        return 2 * (self._width + self._height)
+    def perimeter(self) -> float: return 2 * (self._width + self._height)
 
     @property
-    def width(self) -> float:
-        return self._width
+    def width(self) -> float: return self._width
 
     @property
-    def height(self) -> float:
-        return self._height
+    def height(self) -> float: return self._height
 
     @width.setter
-    def width(self, new_width: float) -> None:
+    def width(self, new_width: Union[int, float]) -> None:
         if not isinstance(new_width, (int, float)):
             raise TypeError(f"Expected float for width, got {type(new_width).__name__}.")
         if new_width <= 0.0:
@@ -80,7 +74,7 @@ class Rectangle(Shape):
         self._width = new_width
 
     @height.setter
-    def height(self, new_height: float) -> None:
+    def height(self, new_height: Union[int, float]) -> None:
         if not isinstance(new_height, (int, float)):
             raise TypeError(f"Expected float for height, got {type(new_height).__name__}.")
         if new_height <= 0.0:
